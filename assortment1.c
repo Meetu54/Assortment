3d59343dd1be1d1b45e6f0039ff24cb8bffd7d82
Meet Savaliya <89479235+Meetu54@users.noreply.github.com>
2023-01-09 03:06:39 -0800

    Update assortment1.c

--- assortment1.c
+++ assortment1.c
@@ -16,14 +16,13 @@ main(){
 	  
 	
 	for(i=0; i<n; i++){
-		if(i<0){
+		if(a[i]<0){
 			
-				printf("%d\n",a[i]<0);
+			
 		}
 		
-		else{
-			printf("");
-		}
+			printf("%d\n",a[i]);
+		
 	
 	}
 	
